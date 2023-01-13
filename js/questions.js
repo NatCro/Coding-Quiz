3c36daefb881ba28286dacc9726d7e9f4b9b7f4d
NatCro <njcrockwell@gmail.com>
2023-01-13 19:40:16 +0000

    added the files

---
+++ js/questions.js
@@ -0,0 +1,43 @@
+var questions = [
+    {
+        questionName: "what does 'js' stand for?", 
+        choices: ["just sauce", "jelly straws", "java script", "jet stream"],
+        correct: "javascript"
+    },
+    {
+        questionName: "what does 'css' stand for?", 
+        choices: ["cascading style sheets", "cats singing songs", "computer sheet scripts", "can't stop singing"],
+        correct: "cascading style sheets"
+    },
+    {
+        questionName: "what does 'html' stand for?", 
+        choices: ["having too much life", "hyper text markdown language", "hyper text markup language", "huge turtles making lunch"],
+        correct: "hyper text markup language"
+    },
+    {
+        questionName: "what does 'md' stand for?", 
+        choices: ["my dad", "mark down", "massive dogs", "make delta"],
+        correct: "mark down"
+    },
+]
+
+// things we need
+
+// its a quiz challenge
+
+//needs start screen with start button
+//      when start button is clicked, we start the timer and show the first question
+//          add event listener to start button
+//need questions
+
+//need choices for each question
+
+//if question is right move to next question
+
+//if question is wrong subtract from timer
+
+//need timer
+
+//we also need high scores
+
+//when its done it prompts to user to put initials
